48618e379e4070d1f1ccddb92b30b40d52499415
Aleph Retamal <retamal.ph@gmail.com>
2017-05-12 14:26:59 +1000

    Add Pods/ directory to .gitignore

--- Pods/Target Support Files/MoyaSugar/MoyaSugar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MoyaSugar : NSObject
-@end
-@implementation PodsDummy_MoyaSugar
-@end
--- Pods/Target Support Files/RxReusable/RxReusable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxReusable : NSObject
-@end
-@implementation PodsDummy_RxReusable
-@end
